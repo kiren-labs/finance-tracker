46cbab4f069260eb98319edc1c04365fd5970c52
Kiren Paul <kirenpaul@gmail.com>
2026-01-14 15:47:12 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,52 @@
+// Service Worker for offline functionality
+const CACHE_NAME = 'finance-tracker-v2';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './robots.txt'
+];
+
+// Install event - cache files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
